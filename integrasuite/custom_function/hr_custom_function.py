0573994ee03be81975de6168220dfb3314de6ee6
Tulasi-111 <tulasi.devi@thimphutechpark.bt>
2026-09-04 20:41:53 +0600

    Travel authorization

---
+++ integrasuite/custom_function/hr_custom_function.py
@@ -0,0 +1,314 @@
+import frappe
+from frappe import _
+from frappe.utils import flt, cint, getdate, date_diff, nowdate
+from frappe.utils.data import get_first_day, get_last_day, add_days
+# from erpnext.custom_utils import get_year_start_date, get_year_end_date
+import json
+import logging
+from datetime import datetime, timedelta
+import datetime
+import calendar
+
+@frappe.whitelist()
+def get_payroll_settings(employee=None):
+		settings = {}
+		if employee:
+			settings = frappe.db.sql("""
+						select
+							e.employee_group,
+							e.grade,
+							d.sws,
+							d.gis,
+							g.health_contribution,
+							g.employee_pf,
+							g.employer_pf
+						from `tabEmployee` e, `tabEmployee Group` g, `tabEmployee Grade` d
+						where e.name = '{}'
+						and g.name = e.employee_group
+						and d.name = e.grade
+				""".format(employee), as_dict=True)
+		settings = settings[0] if settings else frappe._dict()
+		return settings
+
+@frappe.whitelist()
+def get_salary_tax(gross_amt):
+	tax_amount = max_amount = 0
+	max_limit = frappe.db.sql("""select max(b.to_amount)
+		from `tabIncome Tax Slab` a, `tabTaxable Salary Slab` b
+		where now() between a.effective_from and ifnull(a.effective_till, now())
+		and b.parent = a.name
+	""")
+	if not (gross_amt or max_limit):
+		return tax_amount
+	max_amount = flt(max_limit[0][0])
+
+	if flt(gross_amt) > flt(max_amount):
+		tax_amount = ((flt(gross_amt) - 125000.00) * 0.30) + 20208.00
+	else:
+		result = frappe.db.sql("""select ifnull(b.tax,0) from
+			`tabIncome Tax Slab` a, `tabTaxable Salary Slab` b
+			where now() between a.effective_from and ifnull(a.effective_till, now())
+			and b.parent = a.name
+			and %s between ifnull(b.from_amount,0) and ifnull(b.to_amount,0)
+			limit 1
+			""", flt(gross_amt))
+
+		if result:
+			tax_amount = result[0][0]
+
+	return flt(tax_amount)
+
+@frappe.whitelist()
+def get_month_details(year, month):
+	ysd = frappe.db.get_value("Fiscal Year", year, "year_start_date")
+	if ysd:
+		from dateutil.relativedelta import relativedelta
+		import calendar, datetime
+		diff_mnt = cint(month)-cint(ysd.month)
+		if diff_mnt<0:
+			diff_mnt = 12-int(ysd.month)+cint(month)
+		msd = ysd + relativedelta(months=diff_mnt) # month start date
+		month_days = cint(calendar.monthrange(cint(msd.year) ,cint(month))[1]) # days in month
+		med = datetime.date(msd.year, cint(month), month_days) # month end date
+		return frappe._dict({
+			'year': msd.year,
+			'month_start_date': msd,
+			'month_end_date': med,
+			'month_days': month_days
+		})
+	else:
+		frappe.throw(_("Fiscal Year {0} not found").format(year))
+
+@frappe.whitelist()
+def get_basic_and_gross_pay(employee, effective_date):
+	SalaryStructure = frappe.qb.DocType("Salary Structure")
+	SalaryDetail = frappe.qb.DocType("Salary Detail")
+	query = (
+		frappe.qb.from_(SalaryStructure)
+		.join(SalaryDetail)
+		.on(SalaryStructure.name == SalaryDetail.parent)
+		.select(
+			SalaryStructure.net_pay, 
+			SalaryStructure.total_earning, 
+			SalaryDetail.amount.as_("basic_pay")
+		)
+		.where(
+			(SalaryStructure.is_active == "Yes")
+			& (SalaryStructure.employee == employee)
+			& (SalaryDetail.salary_component == "Basic Salary")
+		)
+	)
+	
+	results = query.run(as_dict=True)
+	return results[0] if results else None
+
+
+@frappe.whitelist()
+def get_start_end_dates(fiscal_year, month, company=None):
+	"""Returns dict of start and end dates for given month and fisacl year"""
+
+	months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+	month = str(int(months.index(month))+1).rjust(2, "0")
+
+	start_date = "-".join([str(fiscal_year), month, "01"])
+	end_date   = get_last_day(start_date)
+
+	return frappe._dict({"start_date": start_date, "end_date": end_date})
+
+
+def get_officiating_employee(employee):
+	if not employee:
+		frappe.throw("Employee is Mandatory")
+		
+	qry = "select officiating_employee from `tabOfficiating Employee` where docstatus = 1 and revoked != 1 and %(today)s between from_date and to_date and employee = %(employee)s order by creation desc limit 1"
+	officiate = frappe.db.sql(qry, {"today": nowdate(), "employee": employee}, as_dict=True)
+
+	if officiate:
+		flag = True
+		while flag:
+			temp = frappe.db.sql(qry, {"today": nowdate(), "employee": officiate[0].officiate}, as_dict=True)
+			if temp:
+				officiate = temp
+			else:
+				flag = False
+	return officiate
+
+def post_earned_leaves():	
+	# if not getdate(frappe.utils.nowdate()) == getdate(get_first_day(frappe.utils.nowdate())):
+		
+	# 	return 0
+	
+	date = add_days(frappe.utils.nowdate(), -20)
+	start = get_first_day(date);
+	end = get_last_day(date);
+	from datetime import datetime, timedelta,date
+	today = datetime.today()
+	first_day_of_year = datetime(today.year, 1, 1)
+	last_day_of_year = datetime(today.year, 12, 31)	
+	employees = frappe.db.sql("select name, employee_name,employee_group,date_of_joining from `tabEmployee` where status = 'Active'", as_dict=True)
+	
+	for e in employees:
+		leave_credit=2.5
+		if e.employee_group=='GSP':
+			leave_credit=1.5
+		# print(e.name)
+		if cint(date_diff(end, getdate(e.date_of_joining))) > 14:
+			employee_name = e.name
+			employee_full_name = e.employee_name
+			leave_type = "Earned Leave"
+			from_date = first_day_of_year.strftime(f'%Y-%m-%d')
+			to_date = last_day_of_year.strftime(f'%Y-%m-%d')
+			existing_allocation = frappe.db.exists("Leave Allocation", {
+    			"employee": employee_name,
+    			"leave_type": leave_type,
+    			"from_date": from_date,
+    			"to_date": to_date,
+    			"docstatus": 1  # Check for submitted documents only
+			})
+
+			max_leaves_allowed = flt(
+				frappe.db.get_value("Leave Type", leave_type, "max_leaves_allowed")
+			)
+
+			if existing_allocation:
+				current_year = datetime.now().year
+				first_day = date(current_year, 1, 1).isoformat()
+				last_day = date(current_year, 12, 31).isoformat()
+				
+				la = frappe.get_doc("Leave Allocation", existing_allocation)
+				leave_sum = frappe.get_all(
+											'Leave Ledger Entry',
+											filters={
+        											'leave_type': 'Earned Leave',
+        											'employee': employee_name,
+        											'docstatus': 1,
+        											'from_date': ['between', [first_day, last_day]],
+        											'to_date': ['between', [first_day, last_day]]
+    												},
+											fields=['SUM(leaves) as Leave_sum'],
+											as_list=True
+											)
+
+# Access the result
+				if leave_sum:
+					total_leaves = leave_sum[0][0]
+					#frappe.throw(str(total_leaves))
+					print(f"Total Leaves: {total_leaves}")
+				else:
+					print("No leaves found.")
+				if flt(total_leaves) + flt(leave_credit) <= max_leaves_allowed:
+					la.new_leaves_allocated = flt(la.new_leaves_allocated) + flt(leave_credit)
+					la.save()
+					frappe.db.commit()
+					print(f"Leave Allocation updated successfully for {employee_name}!")
+				# else:
+				# 	frappe.throw(str(la.new_leaves_allocated+2.5))
+			else:
+    
+				la = frappe.new_doc("Leave Allocation")
+				la.employee = employee_name
+				la.employee_name = employee_full_name
+				la.leave_type = leave_type
+				la.from_date = from_date
+				la.to_date = to_date
+				la.carry_forward = cint(1)
+				la.new_leaves_allocated = flt(leave_credit)
+				la.submit()
+				print(f"Leave Allocation submitted successfully for {employee_name}!")
+				
+			# la = frappe.new_doc("Leave Allocation")
+			# la.employee = e.name
+			# la.employee_name = e.employee_name
+			# la.leave_type = "Earned Leave"
+			# la.from_date = first_day_of_year.strftime(f'%Y-%m-%d')
+			# la.to_date = last_day_of_year.strftime(f'%Y-%m-%d')
+			# la.carry_forward = cint(1)
+			# la.new_leaves_allocated = flt(2.5)
+			# la.submit()
+			#print(f"Leave Allocation submitted successfully for {e.name}!")
+		else:
+			pass
+
+#function to get the difference between two dates
+@frappe.whitelist()
+def get_date_diff(start_date, end_date):
+	if start_date is None:
+		return 0
+	elif end_date is None:
+		return 0
+	else:	
+		return frappe.utils.data.date_diff(end_date, start_date) + 1
+
+# @frappe.whitelist()
+# def get_approver(employee):
+# 	deg=frappe.db.get_value("Employee", employee, "designation")
+# 	if not deg:
+# 		frappe.throw("Set designation in employee master")
+# 	if deg=='Chief Executive Officer':
+
+# 		approver = frappe.db.get_value("Employee", employee, "user_id")
+# 		frappe.throw(str(approver))
+# 	else:
+# 		empid = frappe.db.get_value("Employee", employee, "reports_to")
+# 		approver = frappe.db.get_value("Employee", empid, "user_id")
+
+
+# 	return approver
+
+
+@frappe.whitelist()
+def get_reports_to(employee):
+	
+	deg=frappe.db.get_value("Employee", employee, "designation")
+	if not deg:
+		frappe.throw("Set designation in employee master")
+	if deg=='Chief Executive Officer':
+
+		reports_to = frappe.db.get_value("Employee", employee, "user_id")
+		#frappe.throw(str(approver))
+	else:
+		empid = frappe.db.get_value("Employee", employee, "reports_to")
+		reports_to = frappe.db.get_value("Employee", empid, "user_id")
+	
+	return reports_to
+
+
+@frappe.whitelist()  # Allow guest access if needed
+def get_client_ip_api():
+	
+	"""Returns the client's IP when called via API"""
+	if not frappe.request:
+			return {"error": "No request object"}
+
+		# Get the most reliable IP (handles proxies)
+	client_ip = (
+		frappe.request.headers.get("X-Forwarded-For", "").split(",")[0].strip()
+		or frappe.request.headers.get("X-Real-IP", "")
+		or frappe.request.remote_addr
+	)
+
+	return {"client_ip": client_ip}	
+
+
+@frappe.whitelist()
+def is_ip_authorized():
+	#frappe.throw("nd")
+	client_ip = frappe.request.remote_addr
+	#frappe.throw(str(client_ip))
+	if frappe.request.headers.get('X-Real-IP'):
+		frappe.throw("dz")
+	elif frappe.request.headers.get('X-Forwarded-For'):
+		
+		client_ip = frappe.request.headers.get('X-Forwarded-For').split(',')[0]
+		#frappe.throw(str(client_ip))
+		office_ip = frappe.db.get_single_value("HR Settings", "office_gobal_ip")
+		return client_ip == office_ip
+	elif frappe.request.remote_addr:
+		frappe.throw("pl")
+		client_ip = frappe.request.remote_addr
+		start_ip = frappe.db.get_single_value("HR Settings", "office_local_start_ip")
+		end_ip = frappe.db.get_single_value("HR Settings", "office_local_end_ip")
+		return start_ip <= client_ip <= end_ip
+	else:
+		frappe.throw("xx")
+		return false
---
+++ integrasuite/integrasuite_hr/doctype/travel_adjustment/travel_adjustment.py
@@ -0,0 +1,250 @@
+# Copyright (c) 2025, Frappe Technologies Pvt. Ltd. and contributors
+# For license information, please see license.txt
+
+import frappe
+from frappe import _
+from frappe.model.document import Document
+from datetime import timedelta
+from frappe.utils import (
+    add_days,
+    ceil,
+    cint,
+    cstr,
+    date_diff,
+    floor,
+    flt,
+    formatdate,
+    get_first_day,
+    get_last_day,
+    get_link_to_form,
+    getdate,
+    money_in_words,
+    rounded,
+    nowdate,
+)
+from frappe.model.mapper import get_mapped_doc
+from hrms.hr.utils import validate_active_employee
+# from erpnext.custom_workflow import validate_workflow_states, notify_workflow_states
+
+
+class TravelAdjustment(Document):
+    def validate(self):
+        """Validate the document before saving."""
+        validate_active_employee(self.employee)
+        self._validate_travel_last_day()
+        # validate_workflow_states(self)
+
+    def on_update(self):
+        """Check for date overlaps when the document is updated."""
+        self._check_date_overlap()
+
+    def on_submit(self):
+        """Update the linked Travel Authorization when the document is submitted."""
+        self.create_attendance()
+        self._update_travel_authorization()
+
+    def on_cancel(self):
+        """Update the linked Travel Authorization when the document is canceled."""
+        self._update_travel_authorization(cancel=True)
+        self.cancel_attendance()
+
+    def _validate_travel_last_day(self):
+        """Ensure only the last item in the itinerary is marked as the last day."""
+        if self.get("items"):
+            for item in self.items:
+                item.is_last_day = 0
+            self.items[-1].is_last_day = 1
+
+    def create_attendance(self):
+        for row in self.items:
+            from_date = getdate(row.from_date)
+            to_date = getdate(row.to_date) if cint(row.halt) else getdate(row.from_date)
+            noof_days = date_diff(to_date, from_date) + 1
+            for a in range(noof_days):
+                attendance_date = from_date + timedelta(days=a)
+                al = frappe.db.sql(
+                    """select name from tabAttendance 
+						where docstatus = 1 and employee = %s 
+						and attendance_date = %s""",
+                    (self.employee, str(attendance_date)),
+                    as_dict=True,
+                )
+                if al:
+                    doc = frappe.get_doc("Attendance", al[0].name)
+                    doc.cancel()
+
+                # create attendance
+                attendance = frappe.new_doc("Attendance")
+                attendance.flags.ignore_permissions = 1
+                attendance.employee = self.employee
+                attendance.employee_name = self.employee_name
+                attendance.attendance_date = attendance_date
+                attendance.status = "Tour"
+                attendance.branch = self.branch
+                attendance.company = frappe.db.get_value(
+                    "Employee", self.employee, "company"
+                )
+                attendance.reference_name = self.travel_authorization
+                attendance.submit()
+
+    def cancel_attendance(self):
+        if frappe.db.exists(
+            "Attendance", {"reference_name": self.travel_authorization}
+        ):
+            frappe.db.sql(
+                "delete from tabAttendance where reference_name = %s",
+                (self.travel_authorization),
+            )
+
+    def _check_date_overlap(self):
+        """Check for overlapping dates in the itinerary items."""
+        overlap_query = """
+			SELECT t1.idx, t2.idx AS overlap_idx
+			FROM `tabTravel Adjustment Item` t1
+			JOIN `tabTravel Adjustment Item` t2
+			ON t1.parent = t2.parent
+			AND t1.name != t2.name
+			AND t1.from_date <= t2.to_date
+			AND t1.to_date >= t2.from_date
+			WHERE t1.parent = %s
+		"""
+        overlaps = frappe.db.sql(overlap_query, (self.name,), as_dict=True)
+
+        if overlaps:
+            first_overlap = overlaps[0]
+            frappe.throw(
+                _("Row#{}: Dates are overlapping with dates in Row#{}").format(
+                    first_overlap["idx"], first_overlap["overlap_idx"]
+                )
+            )
+
+    def _update_travel_authorization(self, cancel=False):
+        """
+        Update the linked Travel Authorization by deleting and re-inserting items.
+        If `cancel` is True, use `itinerary`; otherwise, use `items`.
+        """
+        try:
+            # Delete existing Travel Authorization Items
+            frappe.db.sql(
+                "DELETE FROM `tabTravel Authorization Item` WHERE parent = %s",
+                (self.travel_authorization,),
+            )
+
+            # Determine which items to insert based on the `cancel` flag
+            items_to_insert = self.itinerary if cancel else self.items
+
+            # Insert new Travel Authorization Items
+            self._insert_travel_authorization_items(items_to_insert)
+
+            # Commit the transaction
+            frappe.db.commit()
+
+        except Exception as e:
+            # Rollback in case of any error
+            frappe.db.rollback()
+            frappe.log_error(f"Error updating Travel Authorization: {e}")
+            raise e
+
+    def _insert_travel_authorization_items(self, items):
+        """Helper method to insert Travel Authorization Items."""
+        for item in items:
+            frappe.get_doc(
+                {
+                    "doctype": "Travel Authorization Item",
+                    "parenttype": "Travel Authorization",
+                    "parentfield": "items",
+                    "idx": item.idx,
+                    "parent": self.travel_authorization,
+                    "from_date": item.from_date,
+                    "to_date": item.to_date,
+                    "halt": item.halt,
+                    "halt_at": item.halt_at,
+                    "travel_from": item.travel_from,
+                    "travel_to": item.travel_to,
+                    "is_last_day": item.is_last_day,
+                }
+            ).insert(ignore_permissions=True)
+
+
+@frappe.whitelist()
+def make_travel_adjustment(source_name, target_doc=None):
+    """
+    Create a Travel Adjustment document from a Travel Authorization.
+    """
+
+    # frappe.throw(str(source_name))
+    def set_missing_values(source, target):
+        """Copy itinerary items from the source Travel Authorization to the target Travel Adjustment."""
+        for item in source.get("items"):
+            target.append("itinerary", item.as_dict())
+
+    doclist = get_mapped_doc(
+        "Travel Authorization",
+        source_name,
+        {
+            "Travel Authorization": {
+                "doctype": "Travel Adjustment",
+                "field_map": {"name": "travel_authorization", 
+                              "employee": "employee",
+                              "approver":"approver",
+                              "approver_name":"approver_name",
+                              "approver_designation":"approver_designation",
+                              "reports_to":"reports_to",
+                              "reports_to_name":"reports_to_name"
+                              },
+                "validation": {
+                    "docstatus": ["=", 1],
+                },
+            },
+            "Travel Authorization Item": {
+                "doctype": "Travel Adjustment Item",
+                "field_map": {
+                    "from_date": "from_date",
+                    "travel_from": "travel_from",
+                    "to_date": "to_date",
+                    "travel_to": "travel_to",
+                },
+            },
+        },
+        target_doc,
+        set_missing_values,
+    )
+
+    return doclist
+
+
+def get_permission_query_conditions(user):
+    if not user:
+        user = frappe.session.user
+
+    user_roles = frappe.get_roles(user)
+    
+    if user == "Administrator":
+        return
+    
+    # Base conditions: owner, employee link, and approver view
+    conditions = f"""
+		(
+			`tabTravel Adjustment`.owner = '{user}'
+			OR
+			EXISTS (
+				SELECT 1 FROM `tabEmployee`
+				WHERE `tabEmployee`.name = `tabTravel Adjustment`.employee
+				AND `tabEmployee`.user_id = '{user}'
+				AND `tabTravel Adjustment`.docstatus != 2
+			)
+			OR
+			(`tabTravel Adjustment`.reports_to = '{user}'
+			 AND `tabTravel Adjustment`.workflow_state NOT IN ('Draft'))
+	)"""
+    # if "Verifier" in user_roles:
+    #     conditions += f"""
+	# 		OR
+	# 		(`tabTravel Adjustment`.reports_to = '{user}'
+	# 		 AND `tabTravel Adjustment`.workflow_state = 'Waiting for Verification')
+	# 	"""
+    # conditions += ")"
+
+    return conditions
+
+    
