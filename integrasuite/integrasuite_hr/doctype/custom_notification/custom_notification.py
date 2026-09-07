0e1a63587f92271381fe497b023dc0c92070bde7
Tulasi-111 <tulasi.devi@thimphutechpark.bt>
2026-09-07 17:11:27 +0600

    notification

--- integrasuite/integrasuite_hr/doctype/custom_notification/custom_notification.py
+++ integrasuite/integrasuite_hr/doctype/custom_notification/custom_notification.py
@@ -2,6 +2,7 @@
 # For license information, please see license.txt
 
 import frappe
+from frappe import _
 from frappe.model.document import Document
 
 
@@ -11,15 +12,21 @@ class CustomNotification(Document):
 def notification_status(doc,email_sender):
 	
 	flag="status"
+	
 	mail_template=get_email_template(doc,flag)
 	#frappe.throw(str(email_template))
 	#frappe.throw("hii here--"+str(email_sender))
 	email_template = frappe.get_doc("Email Template", mail_template)
+
 	parent_doc = frappe.get_doc(doc.doctype, doc.name)
 	args = parent_doc.as_dict()
 	message = frappe.render_template(email_template.response, args)
+
 	recipients = email_sender
+	
+
 	subject = email_template.subject
+	
 	send_mail(recipients,message, subject)
 
 def notification_approver(doc):
@@ -29,6 +36,7 @@ def notification_approver(doc):
 
 	mail_template=get_email_template(doc,flag)
 	email_template = frappe.get_doc("Email Template",mail_template)
+	
 	parent_doc = frappe.get_doc(doc.doctype, doc.name)
 	args = parent_doc.as_dict()
 	message = frappe.render_template(email_template.response, args)
@@ -42,17 +50,18 @@ def notification_approver(doc):
 
 
 def send_mail(recipients, message, subject):
-		#attachments = self.get_attachment()
-		try:
-			frappe.sendmail(
-					recipients=recipients,
-					subject=_(subject),
-					message= _(message),
-					#attachments=attachments,
-				)
-		except:
-			#frappe.throw("hii")
-			pass
+    try:
+        frappe.sendmail(
+            recipients=recipients,
+            subject=_(subject),
+            message=_(message),
+        )
+
+    except Exception:
+        frappe.log_error(
+            frappe.get_traceback(),
+            "Email Sending Error"
+        )
 
 def get_email_template(doc,flag):
 	if flag=="approver":
